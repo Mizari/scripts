822d13496f3403765f4330e1a52e530757c91f50
Mizari <gomizari@gmail.com>
2022-02-04 16:26:52 +0700

    now removes from local types too

--- remove_spaces_from_structs.py
+++ remove_spaces_from_structs.py
@@ -1,4 +1,5 @@
 import ida_struct
+import idaapi
 
 
 def remove_spaces_from_name(sname):
@@ -43,21 +44,40 @@ def remove_spaces_from_name(sname):
 	return sname
 
 
-for i in range(ida_struct.get_struc_qty()):
-	tid = ida_struct.get_struc_by_idx(i)
-	struct_name = ida_struct.get_struc_name(tid)
-	if ' ' not in struct_name:
-		continue
+def remove_spaces_from_structures():
+	for i in range(ida_struct.get_struc_qty()):
+		tid = ida_struct.get_struc_by_idx(i)
+		struct_name = ida_struct.get_struc_name(tid)
+		if ' ' not in struct_name:
+			continue
 
-	new_struct_name = remove_spaces_from_name(struct_name)
-	if new_struct_name == struct_name:
-		print("Failed to remove spaces from", struct_name)
-		continue
+		new_struct_name = remove_spaces_from_name(struct_name)
+		if new_struct_name == struct_name:
+			print("Failed to remove spaces from", struct_name)
+			continue
 
-	if ' ' in new_struct_name:
-		print("Not all spaces are removed from", struct_name, '\n', "Still trying to rename")
+		if ' ' in new_struct_name:
+			print("Not all spaces are removed from", struct_name, '\n', "Still trying to rename")
 
-	rv = ida_struct.set_struc_name(tid, new_struct_name)
-	if not rv:
-		print("Failed to rename", struct_name, '\n', "TO", '\n', new_struct_name, '\n')
-		continue
+		rv = ida_struct.set_struc_name(tid, new_struct_name)
+		if not rv:
+			print("Failed to rename", struct_name, '\n', "TO", '\n', new_struct_name, '\n')
+			continue
+
+def remove_spaces_from_local_types():
+	til = idaapi.get_idati()
+	for i in range(idaapi.get_ordinal_qty(til)):
+		t = idaapi.get_numbered_type(til, i)
+		if t is None: continue
+		type_name = idaapi.get_numbered_type_name(til, i)
+		if ' ' not in type_name: continue
+		new_name = remove_spaces_from_name(type_name)
+		if new_name == type_name: continue
+
+		if ' ' in new_name:
+			print("Not all spaces are removed from", i, type_name, "still trying to rename")
+		idaapi.set_numbered_type(til, i, idaapi.NTF_REPLACE, new_name, t[0])
+	return
+
+remove_spaces_from_structures()
+remove_spaces_from_local_types()
