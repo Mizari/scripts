f1c45ed52374e0fb39dc46b7959d848fb7526e16
Mizari <gomizari@gmail.com>
2022-08-12 12:57:08 +0700

    refactoring2

--- recolour_calls.py
+++ recolour_calls.py
@@ -5,16 +5,17 @@ import idc
 # BGR
 COLOR_CALL = 0x505000
 
-
-if __name__ == "__main__":
+def recolour_calls():
 	next_instr = 0
 	while next_instr != idaapi.BADADDR:
-
 		next_instr = idc.next_head(next_instr)
-
 		insn = idaapi.insn_t()
 		idaapi.decode_insn(insn, next_instr)
 		if idaapi.is_call_insn(insn):
 			idaapi.set_item_color(next_instr, COLOR_CALL)
 
 	idaapi.refresh_idaview_anyway()
+
+
+if __name__ == "__main__":
+	recolour_calls()
